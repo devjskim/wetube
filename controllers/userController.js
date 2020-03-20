3f6fe206e84a46c95a71a9c65e99f4952cee1076
Juseong39 <jskim@smartx.kr>
2020-03-20 17:32:19 +0900

    20200320 commit

---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.send("join");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+export const user = (req, res) => res.send("user");
+export const userDetail = (req, res) => res.send("user detail");
+export const editProfile = (req, res) => res.send("Edit profile");
+export const changePassword = (req, res) => res.send("change password");
---
+++ controllers/videoController.js
@@ -0,0 +1,8 @@
+export const home = (req, res) => res.send("Home");
+export const search = (req, res) => res.send("Search");
+
+export const videos = (req, res) => res.send("video");
+export const upload = (req, res) => res.send("upload");
+export const videoDetail = (req, res) => res.send("video detail");
+export const editVideo = (req, res) => res.send("edit video");
+export const deleteVideo = (req, res) => res.send("delete video");

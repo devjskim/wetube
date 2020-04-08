cb875c268fed4e2b6703a76e8ecdde4da32fd061
Juseong39 <jskim@smartx.kr>
2020-04-08 23:15:33 +0900

    20200408 commit

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
 import userRouter from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
 import globalRouter from "./router/globalRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
+
+import "./passport";
+
 const app = express();
 
 app.use(helmet());
@@ -18,6 +22,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 

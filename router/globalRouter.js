3be7184fdc7cf368b6a730a8a7e60a4f6b533e44
Juseong39 <jskim@smartx.kr>
2020-04-09 20:56:20 +0900

    20200409 commit

--- router/globalRouter.js
+++ router/globalRouter.js
@@ -1,20 +1,31 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
-import { getLogin, postLogin, postJoin, getJoin, logout } from "../controllers/userController";
+import { getLogin, postLogin, postJoin, getJoin, logout, githubLogin, postGithubLogIn } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { onlyPublic } from "../middlewares";
+
 
 const globalRouter = express.Router();
 
 
-globalRouter.get(routes.home, home);
-
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
+globalRouter.get(routes.login, onlyPublic ,getLogin);
 globalRouter.post(routes.login, postLogin);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.logout, onlyPublic ,logout);
 globalRouter.get(routes.search, search);
+
+
+globalRouter.get(routes.gitHub, githubLogin);
+
+globalRouter.get(
+    routes.githubCallback,
+    passport.authenticate('github', { failureRedirect: "/login"}),
+    postGithubLogIn
+);
 
 export default globalRouter;
--- routes.js
+++ routes.js
@@ -20,6 +20,11 @@ const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
+//Github
+
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
 // Object(?)
 const routes = {
     home: HOME,
@@ -61,8 +66,9 @@ const routes = {
         }else{
             return DELETE_VIDEO;
         }
-    }
-
+    },
+    gitHub: GITHUB,
+    githubCallback: GITHUB_CALLBACK
 };
 
 export default routes;
